a1fee330f2d4e0bc0bca48e02760331c93ee8bcf
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2025-09-28 23:30:04 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// Function to open the password modal
+function openPasswordModal() {
+    document.getElementById("passwordModal").style.display = "flex"; // Display modal
+}
+
+// Function to close the password modal
+function closePasswordModal() {
+    document.getElementById("passwordModal").style.display = "none";
+}
+
+// Function to check the password
+function checkPassword() {
+    const passwordInput = document.getElementById("passwordInput").value;
+    const correctPasswords = ["veiled", "Veiled", "VEILED", "$Veiled"]; // Multiple valid passwords
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (correctPasswords.includes(passwordInput)) {
+        document.getElementById("content").classList.remove("hidden");
+        document.body.style.overflowY = 'scroll'; // Enable scrolling
+        closePasswordModal();
+        errorMessage.style.display = "none";
+    } else {
+        errorMessage.style.display = "block";
+    }
+}
+
+// Function to toggle the password visibility
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("passwordInput");
+    const button = document.querySelector("#passwordModal button");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"; // Show password
+        button.textContent = "Hide";
+    } else {
+        passwordInput.type = "password"; // Hide password
+        button.textContent = "Reveal";
+    }
+}
+
+// Add event listener for 'Enter' key to submit password
+document.getElementById("passwordInput").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkPassword();
+    }
+});
